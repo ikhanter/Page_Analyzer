0286cf2ad8d85ce7b1802b91220324a403729cd3
Yulian <khantermail@gmail.com>
2023-09-07 15:51:41 +0300

    fix database.sql

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE urls (
-    id bigint GENERATED ALWAYS AS IDENTITY,
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name character varying(255),
     created_at timestamp,
     scheme character varying(20)
 );
 
 CREATE TABLE url_checks (
-    id bigint GENERATED ALWAYS AS IDENTITY,
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id bigint REFERENCES urls(id),
     status_code integer,
     h1 text,

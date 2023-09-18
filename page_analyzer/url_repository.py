54963772a7eb6e907f9ff631d9b0b18181c74c65
Yulian <khantermail@gmail.com>
2023-09-18 22:59:06 +0300

    fix linter

--- page_analyzer/url_repository.py
+++ page_analyzer/url_repository.py
@@ -78,11 +78,10 @@ class UrlRepository:
                     %(description)s,
                     %(created_at)s);
                     COMMIT;''',
-                    {
-                        'url_id': url_id,
-                        'status_code': status_code,
-                        'h1': h1,
-                        'title': title,
-                        'description': description,
-                        'created_at': created_at,
+                    {'url_id': url_id,
+                     'status_code': status_code,
+                     'h1': h1,
+                     'title': title,
+                     'description': description,
+                     'created_at': created_at,
                     }, get_back=False)
